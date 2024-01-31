d9765f85233bad887d10e0419b053bc35b3113d8
killroy2k <83260071+killroy2k@users.noreply.github.com>
2024-01-31 15:03:16 -0500

    removing files and updating check_nvd parameter

--- project/project/testnvd.py
+++
@@ -1,9 +0,0 @@
-import requests, openai, csv, tweepy, sqlite3, os, smtplib, json
-from datetime import datetime, timedelta
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-from project import *
-
-check_nvd(1)
